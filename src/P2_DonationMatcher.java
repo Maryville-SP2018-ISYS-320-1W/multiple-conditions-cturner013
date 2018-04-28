a358fdc1dec5fbcb8601cf26c85f7af28df56ed6
Cturner013 <cturner7@live.maryville.edu>
2018-04-28 12:07:34 -0500

    Car

--- src/P2_DonationMatcher.java
+++ src/P2_DonationMatcher.java
@@ -8,22 +8,19 @@ public class P2_DonationMatcher {
 		int donationsCount = 20;
 		Scanner console = new Scanner(System.in);
 		System.out.print("Will you donation be multiplied 1 or 2? ");
+		
+
 		int multiplier = console.nextInt();
-
-		if (multiplier == 1) {
-			System.out.print("And how much are you donating? ");
-			int donation = console.nextInt();
-			donationsSum = donationsSum + donation;
-			donationsCount++;
+		System.out.print("And how much are you donating? ");
+		int donation = console.nextInt();
+		donationsSum = donationsSum + donation;
+		donationsCount++;
+		
+		if (multiplier ==1) {
 		}
-
 		if (multiplier == 2) {
-			System.out.print("And how much are you donating? ");
-			int donation = console.nextInt();
-			donationsSum = donationsSum + 2 * donation;
-			donationsCount++;
+			
 		}
-
 		System.out.println("Total donated sum: " + donationsSum);
 		System.out.println("Total number of donations: " + donationsCount);
 	}
